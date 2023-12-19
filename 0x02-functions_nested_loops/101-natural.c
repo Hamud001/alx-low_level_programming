55c4690cf3823ea8912f41c925a0ca6f3bb98aa6
Hamud <hamudalwi91@gmail.com>
2023-12-19 11:16:29 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,25 +1,30 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
- * followed by a new line
+ * main - computes and prints the sum of all the multiples
+ * of 3 or 5 below 1024
+ * Return: Always 0 (Success)
  */
-void print_alphabet_x10(void)
+int main(void)
 {
-	char ch;
+	unsigned long int sum3, sum5, sum;
 	int i;
 
-	i = 0;
+	sum3 = 0;
+	sum5 = 0;
+	sum = 0;
 
-	while (i < 10)
+	for (i = 0; i < 1024; ++i)
 	{
-		ch = 'a';
-		while (ch <= 'z')
+		if ((i % 3) == 0)
 		{
-			_putchar(ch);
-			ch++;
+			sum3 = sum3 + i;
+		} else if ((i % 5) == 0)
+		{
+			sum5 = sum5 + i;
 		}
-		_putchar('\n');
-		i++;
 	}
+	sum = sum3 + sum5;
+	printf("%lu\n", sum);
+	return (0);
 }
